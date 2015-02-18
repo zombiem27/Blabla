897c5b575c04bc68f581cfbeaf3e18fe3ee6c1c9
zombiem27 <kaitokun16@gmail.com>
2015-02-18 17:49:47 +0700

    versi 1.1.0

--- form.php
+++ form.php
@@ -3,11 +3,29 @@
     <title>Cuma cumi</title>
 </head>
 <body>
-    <h1>Aplikasi gak jelas</h1>
+    <h1 align="center">Aplikasi gak jelas</h1>
 	<form method="post" action="form_act.php">
-	   Nama:<input type="number" name="nama"/><br/>
-	   Alamat:<input type="text" name="alamat" size="50"/><br/>
-	   <input type="submit" value="Proses!!!" name="proses"/>
+	<table width="452" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#9AC2E3">
+	   <tr>
+			<td height="40" align="center" bgcolor="#9AC2E3"><strong><font color="#FFFFFF">Entry</font></strong></td>
+	   </tr>
+	   <tr>
+			<td bgcolor="#FFFFFF"><table width="452" border="0" align="center" cellpadding="5" cellspacing="0">
+	   </tr>
+	   <tr>
+			<td>Nama</td> <td>:</td>
+			<td><input type="text" name="nama" size="30" maxlength="30"/></td>
+		</tr>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td><textarea name="Alamat" cols="30" rows="5"></textarea></td>
+		</tr>
+		<tr>
+			<td colspan="3" align="center"> <input type="submit" value="OK" name="proses"/> </td>
+		</tr>
+			</table></td>
+	</table>
 	</form>
 </body>
 </html>

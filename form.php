37a3ad49354f80996c76ad9a6ba9246ca3ac22c3
zombiem27 <kaitokun16@gmail.com>
2015-02-18 12:50:27 +0700

    versi 1.0.0

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Cuma cumi</title>
+</head>
+<body>
+    <h1>Aplikasi gak jelas</h1>
+	<form method="post" action="form_act.php">
+	   Nama:<input type="number" name="nama"/><br/>
+	   Alamat:<input type="text" name="alamat" size="50"/><br/>
+	   <input type="submit" value="Proses!!!" name="proses"/>
+	</form>
+</body>
+</html>
